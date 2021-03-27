b14e08aeb47f3c92c962263a24d613f0f36738f9
djamel <djamel.sbar@gmail.com>
2021-03-27 22:07:18 +0100

    some modifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
 import 'quiz.dart';
 import 'result.dart';
+
 main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int _questionIndex = 0; 
+  int _questionIndex = 0;
   final List<Map<String, Object>> _questions = [
     {
-      "questionText":"What\'s Your favurite color?",
-      "answer":['Black','Bleu','Orange','Green']
+      "questionText": "What\'s Your favurite color?",
+      "answer": ['Black', 'Bleu', 'Orange', 'Green']
     },
     {
-      "questionText":"What\'s Your favurite Animal?",
-      "answer":['Cat','Sheep','Dog','Wolf']
+      "questionText": "What\'s Your favurite Animal?",
+      "answer": ['Cat', 'Sheep', 'Dog', 'Wolf']
     },
     {
-      "questionText":"What\'s Your favurite language?",
-      "answer":['En','Ar','Fr','Al']
+      "questionText": "What\'s Your favurite language?",
+      "answer": ['En', 'Ar', 'Fr', 'Al']
     },
     {
-      "questionText":"What\'s Your favurite Country?",
-      "answer":['Algeria','USA','France','England']
-    }];
+      "questionText": "What\'s Your favurite Country?",
+      "answer": ['Algeria', 'USA', 'France', 'England']
+    }
+  ];
 
-  void anserQuestion(){
+  void anserQuestion() {
     setState(() {
       _questionIndex += 1;
     });
@@ -38,7 +41,7 @@ class _MyAppState extends State<MyApp> {
     print("answer Chosen !");
   }
 
-  void restart(){
+  void restart() {
     setState(() {
       _questionIndex = 0;
     });
@@ -47,15 +50,26 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar:AppBar(
-          title: Text("Quiz App"),
-        ),
-        body: Container(
-          //width: double.infinity, // in order to it takes all widh screen
-          child: _questionIndex < _questions.length ? Quiz(_questions,_questionIndex,anserQuestion): Result(restart)
-        ),
-      )      
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Quiz App"),
+          ),
+          body: Container(
+              //width: double.infinity, // in order to it takes all widh screen
+              child: _questionIndex < _questions.length
+                  ? Quiz(_questions, _questionIndex, anserQuestion)
+                  : Result(restart)),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.arrow_back),
+            onPressed: () {
+              setState(() {
+                if (_questionIndex > 0) {
+                  _questionIndex -= 1;
+                }
+              });
+            },
+          ),
+        ));
   }
 }
